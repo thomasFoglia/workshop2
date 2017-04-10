cdbe65a5b1b54a91fe8410d8a0b5f6c03839d486
Thomas Foglia <thomas.foglia38@gmail.com>
2017-04-10 16:15:41 +0200

    jj

--- game.php
+++ game.php
@@ -3,12 +3,30 @@ include "header.php"; ?>
 <link href="css/game.css" rel="stylesheet">
 
 <div class="column thin">
-  SCORE
+  <div class="uk-card uk-card-default uk-card-body uk-animation-slide-left-medium">
+      <div class="uk-card-header">
+          <div class="uk-grid-small uk-flex-middle" uk-grid>
+              <div class="uk-width-auto">
+                  <img class="uk-border-circle" width="40" height="40" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Avatar_poe84it.png">
+              </div>
+              <div class="uk-width-expand">
+                  <h3 class="uk-card-title uk-margin-remove-bottom">User1</h3>
+                  <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">score</time></p>
+              </div>
+          </div>
+      </div>
+      <div class="uk-card-body">
+          <p>ssdnfkdskf</p>
+      </div>
+      <div class="uk-card-footer">
+          <a href="#" class="uk-button uk-button-text">Read more</a>
+      </div>
+  </div>
 </div>
 <div class='column large' id='grid'>
   <?php
   for ($col = 0; $col < 20; $col++) { ?>
-    <div style="display: inline-block; float: left; width: 31px;">
+    <div class="ligne">
       <?php
       for ($ligne = 0; $ligne < 20; $ligne++) { ?>
         <div class='cellule' id='cellule_<?php echo $col . "_" . $ligne;?>'></div>
@@ -17,7 +35,25 @@ include "header.php"; ?>
     <?php }?>
 </div>
 <div class="column thin">
-  SCORE
+    <div class="uk-card uk-card-default uk-card-body uk-animation-slide-right-medium">
+      <div class="uk-card-header">
+          <div class="uk-grid-small uk-flex-middle" uk-grid>
+              <div class="uk-width-auto">
+                  <img class="uk-border-circle" width="40" height="40" src="http://www.freeiconspng.com/uploads/face-avatar-png-14.png">
+              </div>
+              <div class="uk-width-expand">
+                  <h3 class="uk-card-title uk-margin-remove-bottom">User2</h3>
+                  <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">score</time></p>
+              </div>
+          </div>
+      </div>
+      <div class="uk-card-body">
+          <p>ssdnfkdskf</p>
+      </div>
+      <div class="uk-card-footer">
+          <a href="#" class="uk-button uk-button-text">Read more</a>
+      </div>
+  </div>
 </div>
 <?php include "footer.php";?>
 
--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    // sur un "précédent"
+    if ($("select[name='playerType']").val() != "ia") {
+        $("#iaAddress").hide();
+    }
+
     $("select[name='playerType']").change(function() {
         if ($("select[name='playerType']").val() == "ia") {
             $("#iaAddress").show();
