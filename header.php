a2eb9b496b9656dac75fad75a63208a742681125
Yohan Dahmani <yohan.dahmani@gmail.com>
2017-04-10 11:57:51 +0200

    Init libs
    Structure etc

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Yohan Dahmani</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bower_components/uikit/dist/css/uikit.min.css" rel="stylesheet">
+    <link href="css/normalize.css" rel="stylesheet">
+    <link href="css/global.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/uikit/dist/js/uikit.min.js"></script>
+    <script src="bower_components/uikit/dist/js/uikit-icons.min.js"></script>
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+include "header.php";
+?>
+
+<div class="uk-background-cover uk-background-fixed uk-background-center-center uk-height-large" style="background-image: url(img/material-wpp2.jpg);">
+    <div class="uk-section">
+        <div class="uk-container" uk-scrollspy="target: > div; cls:uk-animation-kenburns;">
+            <div class="uk-text-center huge-title" style="color: #37474f">Groupe</div>
+            <div class="uk-text-center uk-text-large roboto-thin" style="color: #fff">Nous sommes le groupe</div>
+        </div>
+    </div>
+</div>
+
+<div class="uk-section uk-section-muted">
+    <div class="uk-container">
+        <div class="uk-grid" uk-grid>
+            <div>Bonjour</div>
+            <div>Bonjour</div>
+            <div>Bonjour</div>
+            <div>Bonjour</div>
+        </div>
+    </div>
+</div>
+
+<div class="uk-section uk-section-muted">
+    <div class="uk-container">
+        <div class="uk-grid" uk-grid>
+            <div>Bonjour</div>
+            <div>Bonjour</div>
+            <div>Bonjour</div>
+            <div>Bonjour</div>
+        </div>
+    </div>
+</div>
+
+<div class="uk-section uk-section-muted">
+    <div class="uk-container">
+        <div class="uk-grid" uk-grid>
+            <div>Bonjour</div>
+            <div>Bonjour</div>
+            <div>Bonjour</div>
+            <div>Bonjour</div>
+        </div>
+    </div>
+</div>
+
+
+<?php include "footer.php";?>
