84995b910bc0407af2dda337d88982a79a6677e4
Thomas Foglia <thomas.foglia38@gmail.com>
2017-04-10 15:40:25 +0200

    dsfd

--- game.php
+++ game.php
@@ -1,25 +1,27 @@
-<?php include "header.php"; ?>
+<?php
+include "header.php"; ?>
 <link href="css/game.css" rel="stylesheet">
 
-<div>
-  <div class="column thin">
-    SCORE
-  </div>
-  <div class="column large" id="grid">
-    <?php
-    $i = 0;
-    for ($i; $i < 20; $i++) {
+<div class="column thin">
+  SCORE
+</div>
+<div class='column large' id='grid'>
+  <?php
+  for ($i = 0; $i < 20; $i++) { ?>
+    <div style="display: inline-block; float: left; width: 31px;">
+      <?php
       $p = 0;
       for ($p; $p < 20; $p++) { ?>
-        <div class='cellule' id='cellule<?php echo $i . "_" . $p?>'>&nbsp;</div>
-        <?php
-      }
-    } ?>
-  </div>
-  <div class="column thin">
-    SCORE
-  </div>
+        <div class='cellule' id='cellule_<?php echo $i . "_" . $p;?>'></div>
+        <?php } ?>
+    </div>
+    <?php }?>
 </div>
+<div class="column thin">
+  SCORE
+</div>
+<?php include "footer.php";?>
+
 
 <script>
   $(document).ready(function() {
