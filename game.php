246b9dadabd328952dfaeb09d4be92c8566780a8
Thomas Foglia <thomas.foglia38@gmail.com>
2017-04-10 14:48:23 +0200

    nsdnfj ezif zjffjoi

---
+++ game.php
@@ -0,0 +1,29 @@
+<?php include "header.php"; ?>
+
+<div>
+  <div class="column thin">
+    SCORE
+  </div>
+  <div class="column large" id="grid">
+    <?php
+    $i = 0;
+    for ($i; $i < 20; $i++) {
+      $p = 0;
+      for ($p; $p < 20; $p++) { ?>
+        <div class='cellule' id='cellule<?php echo $i . "_" . $p?>'>&nbsp;</div>
+        <?php
+      }
+    } ?>
+  </div>
+  <div class="column thin">
+    SCORE
+  </div>
+</div>
+
+<script>
+  $(document).ready(function() {
+    $( "#grid .cellule" ).hover(function(e) {
+      console.log(e.target.id);
+    });
+  });
+</script>
