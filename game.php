34d5afd14e202b261f50248f5e9431cac94254fa
Yohan Dahmani <yohan.dahmani@gmail.com>
2017-04-11 15:05:28 +0200

    Starfffuulehhhh GAME JS
    GAME JS

--- game.php
+++ game.php
@@ -1,59 +1,160 @@
 <?php
-include "header.php"; ?>
-<link href="css/game.css" rel="stylesheet">
-<script src="js/game.js"></script>
+include "header.php"; 
+if(!empty($_GET["urlServer"]))
+{
+    $serverUrl = $_GET["urlServer"];
+}
+else {
+    //POUR LE DEV ENLEVER APRES
+    $serverUrl = "http://monserver.fr/";
+}
 
-<div class="column thin" style="margin-right: 30px; padding-top: 10px;">
-  <div class="uk-card uk-card-default uk-card-body uk-animation-slide-left-medium">
-      <div class="uk-card-header">
-          <div class="uk-grid-small uk-flex-middle" uk-grid>
-              <div class="uk-width-auto">
-                  <img class="uk-border-circle" width="40" height="40" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Avatar_poe84it.png">
-              </div>
-              <div class="uk-width-expand">
-                  <h3 class="uk-card-title uk-margin-remove-bottom">User1</h3>
-                  <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">score</time></p>
-              </div>
-          </div>
-      </div>
-      <div class="uk-card-body">
-          <p>ssdnfkdskf</p>
-      </div>
-      <div class="uk-card-footer">
-          <a href="#" class="uk-button uk-button-text">Read more</a>
-      </div>
-  </div>
-</div>
-<div class='column large' id='grid'>
-  <?php
-  for ($col = 0; $col < 19; $col++) { ?>
-    <div class="ligne">
-      <?php
-      for ($ligne = 0; $ligne < 19; $ligne++) { ?>
-        <div class='cellule' id='cellule_<?php echo $col . "_" . $ligne;?>' style="background: url('img/cross.png')"></div>
-        <?php } ?>
+if(!empty($_GET["playerName"]))
+{
+    $playerName = $_GET["playerName"];
+}
+else {
+    //POUR LE DEV ENLEVER APRES
+    $playerName = "http://monserver.fr/";
+}
+?>
+    <link href="css/game.css" rel="stylesheet">
+    <script src="js/game.js?v=<?=time();?>"></script>
+    <script>
+    $(document).ready(function(){
+        var tenaillesPlayer1 = 0;
+        var tenaillesPlayer2 = 0;
+        var lastX = -99;
+        var lastY = -99;
+        var numTour = 0;
+
+        setInterval(function() {
+            $.get("<?=$serverUrl?>/turn/<?=$playerName?>", function(result){
+                //Ce n'est pas a nous de jouer
+                if(result.status == 0) {
+                    $("#player2cardFooter").show();
+                    $("#waitingPlayer2").show();
+                    $("#player1turn").hide();
+                }
+                else {
+                    $("#player2cardFooter").hide();
+                    $("#waitingPlayer2").hide();
+                    $("#player1turn").show();
+                }
+
+                //Le nombre de tenailles a changé
+                if(result.nbTenaillesJ1 != tenaillesPlayer1) {
+                    $("#tenaillesPlayer1").html(tenaillesPlayer1);
+                    $("#player1HP").val(100 - (tenaillesPlayer1 * 10));
+                }
+
+                if(result.nbTenaillesJ2 != tenaillesPlayer2) {
+                    $("#tenaillesPlayer2").html(tenaillesPlayer2);
+                    $("#player2HP").val(100 - (tenaillesPlayer2 * 10));
+                }
+
+                //Un joueur a posé un pion
+                if (result.dernierCoupX != lastX || result.dernierCoupY != lastY){
+                    lastX = result.dernierCoupX;
+                    lastY = result.dernierCoupY;  
+                    addPt2(lastX, lastY);
+                }
+
+                //Prolongation
+                if (result.prolongation == "true") {
+                    //Afficher un truc à l'écran
+                }
+
+                //Fin de la partie
+                if (result.finPartie == true) {
+                    //Envoyer en post result.detailFinPartie
+                    href.location = "end.php";
+                }
+
+                if (result.numTour != numTour) {
+                    $("#nbTour").html("Tour " + numTour);
+                }
+            });
+        }, 500);
+    });
+    
+    </script>
+
+    <div class="column thin" style="margin-right: 30px; padding-top: 10px;">
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-header">
+                <div class="uk-grid-small uk-flex-middle" uk-grid>
+                    <div class="uk-width-auto">
+                        <img class="uk-border-circle" width="40" height="40" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Avatar_poe84it.png">
+                    </div>
+                    <div class="uk-width-expand">
+                        <h3 class="uk-card-title uk-margin-remove-bottom">Joueur 1</h3>
+                        <p class="uk-text-meta uk-margin-remove-top">
+                            Tenailles : <span id="tenaillesPlayer1">0</span>
+                        </p>
+                    </div>
+                </div>
+                <progress id="player1HP" class="uk-progress" value="100" max="100"></progress>
+            </div>
+            <div class="uk-card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div id="player1cardFooter" class="uk-card-footer" style="display: none;">
+                <div id="waitingPlayer1" style="display: none; align-items: center; justify-content: center;">
+                    <div uk-spinner></div>
+                    <div style="margin-left: 15px;">En attente du joueur</div>    
+                </div>
+                
+                <div id="player1turn" style="display: none; align-items: center; justify-content: center;">
+                    <span class="uk-margin-small-right" uk-icon="icon: bell;"></span>
+                    <div style="margin-left: 15px;">À ton tour</div>    
+                </div>
+            </div>
+        </div>
+
+        <div><h1 id="nbTour" class="uk-heading-line uk-text-center"><span>Tour 0</span></h1></div>
     </div>
-    <?php }?>
-</div>
-<div class="column thin" style="margin-left: 30px; padding-top: 10px;">
-    <div class="uk-card uk-card-default uk-card-body uk-animation-slide-right-medium">
-      <div class="uk-card-header">
-          <div class="uk-grid-small uk-flex-middle" uk-grid>
-              <div class="uk-width-auto">
-                  <img class="uk-border-circle" width="40" height="40" src="http://www.freeiconspng.com/uploads/face-avatar-png-14.png">
-              </div>
-              <div class="uk-width-expand">
-                  <h3 class="uk-card-title uk-margin-remove-bottom">User2</h3>
-                  <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">score</time></p>
-              </div>
-          </div>
-      </div>
-      <div class="uk-card-body">
-          <p>ssdnfkdskf</p>
-      </div>
-      <div class="uk-card-footer">
-          <a href="#" class="uk-button uk-button-text">Read more</a>
-      </div>
-  </div>
-</div>
-<?php include "footer.php";?>
+    <div class='column large' id='grid'>
+        <?php
+        for ($col = 0; $col < 19; $col++) { ?>
+            <div class="ligne">
+            <?php
+                for ($ligne = 0; $ligne < 19; $ligne++) { ?>
+                    <div class='cellule' id='cellule_<?php echo $col . "_" . $ligne;?>' style="background: url('img/cross.png')"></div>
+                <?php } ?>
+            </div>
+        <?php }?>
+    </div>
+    <div class="column thin" style="margin-left: 30px; padding-top: 10px;">
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-header">
+                <div class="uk-grid-small uk-flex-middle" uk-grid>
+                    <div class="uk-width-auto">
+                        <img class="uk-border-circle" width="40" height="40" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Avatar_poe84it.png">
+                    </div>
+                    <div class="uk-width-expand">
+                        <h3 class="uk-card-title uk-margin-remove-bottom">Joueur 1</h3>
+                        <p class="uk-text-meta uk-margin-remove-top">
+                            <time datetime="2016-04-01T19:00">SCORE</time>
+                        </p>
+                    </div>
+                </div>
+                <progress id="player2HP" class="uk-progress" value="100" max="100" style="background: red"></progress>
+            </div>
+            <div class="uk-card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div id="player2cardFooter" class="uk-card-footer" style="display: none;">
+                <div id="waitingPlayer2" style="display: none; align-items: center; justify-content: center;">
+                    <div uk-spinner></div>
+                    <div style="margin-left: 15px;">En attente du joueur</div>    
+                </div>
+                
+                <div id="player2turn" style="display: none; align-items: center; justify-content: center;">
+                    <span class="uk-margin-small-right" uk-icon="icon: bell;"></span>
+                    <div style="margin-left: 15px;">Au jour du joueur 2</div>    
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "footer.php";?>
--- js/game.js
+++ js/game.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
-  $(".cellule").on("mouseleave", function() {
-    $(".cellule > .temp_circle").remove();
-  });
+    $(".cellule").on("mouseleave", function() {
+        $(".cellule > .temp_circle").remove();
+    });
 
-  $(".cellule").on("mouseenter", function() {
-    $("#" + this.id).append('<img src="img/circley.png" class="temp_circle" width="15" height="15" style="margin-left: auto; margin-right: auto; display: block;position: absolute;"/>');
-  });
+    $(".cellule").on("mouseenter", function() {
+        $("#" + this.id).append('<img src="img/circley.png" class="temp_circle" width="15" height="15" style="margin-left: auto; margin-right: auto; display: block;position: absolute;"/>');
+    });
 
-  $(".cellule").on("click", function() {
-    cell_split = this.id.split('_');
-    addPt1(cell_split[1], cell_split[2]);
-    $(this).find('.temp_circle').removeAttr('class');
-  });
+    $(".cellule").on("click", function() {
+        cell_split = this.id.split('_');
+        addPt1(cell_split[1], cell_split[2]);
+        $(this).find('.temp_circle').removeAttr('class');
+    });
 });
 
 function addPt1(x, y) {
-  if ($("#cellule_" + x + "_" + y).is(":empty")) {
-    $("#cellule_" + x + "_" + y).append('<img src="img/circleb.png" width="15" height="15" style="margin-left: auto; margin-right: auto; display: block;position: absolute;"/>');
-  }
+    if ($("#cellule_" + x + "_" + y).is(":empty")) {
+        $("#cellule_" + x + "_" + y).append('<img src="img/circleb.png" width="15" height="15" style="margin-left: auto; margin-right: auto; display: block;position: absolute;"/>');
+    }
 }
 
 function addPt2(x, y) {
-  if ($("#cellule_" + x + "_" + y).is(":empty")) {
-    $("#cellule_" + x + "_" + y).append('<img src="img/circley.png" width="15" height="15" style="margin-left: auto; margin-right: auto; display: block;position: absolute;"/>');
-  }
+    if ($("#cellule_" + x + "_" + y).is(":empty")) {
+        $("#cellule_" + x + "_" + y).append('<img src="img/circley.png" width="15" height="15" style="margin-left: auto; margin-right: auto; display: block;position: absolute;"/>');
+    }
 }
 
 function removePt(x, y) {
