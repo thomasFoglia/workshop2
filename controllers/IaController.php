7c714ac4d9babdd42de768ab5a9ca61ddb4eb9da
Loïc Mazallon <loic.mazallon@gmail.com>
2017-04-11 16:21:42 +0200

    Ia optimized 2

--- controllers/IaController.php
+++ controllers/IaController.php
@@ -4,30 +4,30 @@ class IaController
 {
     public function getAction($request) {
     	if( isset($_POST['currentGrid']) ){
-      $currentGrid = $_POST['currentGrid'];
-  	}
-      $data = [];
-      if(isset($request->url_elements[2]) && $request->url_elements[2] != '') {
-        $data = $request->url_elements[2];
-
-        $tabEmpty = [];
-		foreach ($currentGrid as $key=>$line){
-			foreach($line as $sskey => $element){			
-				switch ($element) {
-				    case 0:
-						$tabEmpty[] = ['x' => $sskey , 'y' => $key]; 
-				    case 1:
-				        $tabJoueur1[] = ['x' => $sskey , 'y' => $key]; 
-				    case 2:
-				        $tabJoueur2[] = ['x' => $sskey , 'y' => $key]; 
+      		$currentGrid = $_POST['currentGrid'];
+  		}
+      	$data = [];
+      	if(isset($request->url_elements[2]) && $request->url_elements[2] != '') {
+	        $data = $request->url_elements[2];
+	        $tabEmpty = [];
+			foreach ($currentGrid as $key=>$line){
+				foreach($line as $sskey => $element){			
+					switch ($element) {
+					    case 0:
+							$tabEmpty[] = ['x' => $sskey , 'y' => $key]; 
+					    case 1:
+					        $tabJoueur1[] = ['x' => $sskey , 'y' => $key]; 
+					    case 2:
+					        $tabJoueur2[] = ['x' => $sskey , 'y' => $key]; 
+					}
 				}
 			}
-		}
-		$randomChoice = array_rand($tabVide);
-		$data = json_encode($tabVide[$randomChoice]);
-      header("HTTP/1.1 200 OK");
-      return $data;
-    }
+			$randomChoice = array_rand($tabVide);
+			$data = json_encode($tabVide[$randomChoice]);
+	      	header("HTTP/1.1 200 OK");
+	      return $data;
+    	}
+	}
 }
 
 ?>
