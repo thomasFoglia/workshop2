b6eee5f85c7797eb65152732f94ff9b29460cba9
Loïc Mazallon <loic.mazallon@gmail.com>
2017-04-11 16:16:51 +0200

    Ia random optimized

--- controllers/IaController.php
+++ controllers/IaController.php
@@ -2,22 +2,29 @@
 
 class IaController
 {
-    public function getAction($request,$current_grid) {
+    public function getAction($request) {
+    	if( isset($_POST['currentGrid']) ){
+      $currentGrid = $_POST['currentGrid'];
+  	}
       $data = [];
       if(isset($request->url_elements[2]) && $request->url_elements[2] != '') {
         $data = $request->url_elements[2];
 
-
-        $tabresult = [];
-		foreach ($current_grid as $key=>$line){
+        $tabEmpty = [];
+		foreach ($currentGrid as $key=>$line){
 			foreach($line as $sskey => $element){			
-				if ($element == 0){
-					$tabresult[] = ['x' => $sskey , 'y' => $key];
+				switch ($element) {
+				    case 0:
+						$tabEmpty[] = ['x' => $sskey , 'y' => $key]; 
+				    case 1:
+				        $tabJoueur1[] = ['x' => $sskey , 'y' => $key]; 
+				    case 2:
+				        $tabJoueur2[] = ['x' => $sskey , 'y' => $key]; 
 				}
 			}
 		}
-		$randomChoice = array_rand($tabresult);
-		$data = json_encode($tabresult[$randomChoice]);
+		$randomChoice = array_rand($tabVide);
+		$data = json_encode($tabVide[$randomChoice]);
       header("HTTP/1.1 200 OK");
       return $data;
     }
