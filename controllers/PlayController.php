a816dbf37f8c3e427cdecaec3d93b11eb9679365
Thomas Foglia <thomas.foglia38@gmail.com>
2017-04-11 10:20:24 +0200

    maj api

---
+++ controllers/PlayController.php
@@ -0,0 +1,15 @@
+<?php
+
+class PlayController
+{
+    public function getAction($request) {
+      $data = [];
+      if(isset($request->url_elements[2]) && $request->url_elements[2] != '') {
+        $data = $request->url_elements[2];
+      }
+      header("HTTP/1.1 200 OK");
+      return $data;
+      }
+}
+
+?>
