46a76e4e30a10a1e0b782fc61eac86c18b14b1be
Pascolien <prince.matsika@epsi.fr>
2017-04-11 13:29:00 +0200

    blala ajax blabla
    blablabal

--- js/index.js
+++ js/index.js
@@ -11,4 +11,30 @@ $(document).ready(function() {
             $("#iaAddress").hide();
         }
     });
+
+    $(form).submit(function(){
+       $.ajax({
+            url: url,
+            type: GET,
+            data: data,
+            dataType: JSON,
+            success: function(param){
+            param = json.parse(param);
+            code = param["code"];
+               switch(code){
+                case 200:
+                    window.location.replace("game.php");
+                    break;
+                case 401:
+                    //connection non autorisée
+                    break;
+                
+                }
+            }
+         
+        });  
+       
+
+
+    });
 });
