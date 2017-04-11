dd514e86265819d80d21fb10b49d4379ef09bfd1
Loïc Mazallon <loic.mazallon@gmail.com>
2017-04-11 15:32:27 +0200

    Ia random

--- controllers/IaController.php
+++ controllers/IaController.php
@@ -2,11 +2,22 @@
 
 class IaController
 {
-    public function getAction($request) {
+    public function getAction($request,$current_grid) {
       $data = [];
       if(isset($request->url_elements[2]) && $request->url_elements[2] != '') {
         $data = $request->url_elements[2];
-      }
+
+
+        $tabresult = [];
+		foreach ($current_grid as $key=>$line){
+			foreach($line as $sskey => $element){			
+				if ($element == 0){
+					$tabresult[] = ['x' => $sskey , 'y' => $key];
+				}
+			}
+		}
+		$randomChoice = array_rand($tabresult);
+		$data = json_encode($tabresult[$randomChoice]);
       header("HTTP/1.1 200 OK");
       return $data;
     }
