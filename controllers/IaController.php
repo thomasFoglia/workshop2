e93324b4f2638d6a735cb1f828752d1010e1c721
Loïc Mazallon <loic.mazallon@gmail.com>
2017-04-12 11:16:08 +0200

    ia

--- controllers/IaController.php
+++ controllers/IaController.php
@@ -5,34 +5,29 @@ class IaController
 		$data = "penis";
     	if( isset($_POST['currentGrid']) ){
       		$currentGrid = $_POST['currentGrid'];
-  		 $data ="current grid passe bien";
-  		}
-  		else{
-  			$data = "on recoit pas le post";
-  		}
-      //	$data = [];
-      	if(isset($request->url_elements[2]) && $request->url_elements[2] != '') {
-	       // $data = $request->url_elements[2];
-	        $tabEmpty = [];
+      		//$currentGrid = unserialize($currentGrid);
+      			        $tabEmpty = [];
 			foreach ($currentGrid as $key=>$line){
 				foreach($line as $sskey => $element){
 					switch ($element) {
 					    case 0:
 							$tabEmpty[] = ['x' => $sskey , 'y' => $key];
-							break;
 					    case 1:
 					        $tabJoueur1[] = ['x' => $sskey , 'y' => $key];
-					        break;
 					    case 2:
 					        $tabJoueur2[] = ['x' => $sskey , 'y' => $key];
-					        break;
 					}
 				}
 			}
 			$randomChoice = array_rand($tabEmpty);
-			//$data = json_encode($tabEmpty[$randomChoice]);
+			$data = json_encode($tabEmpty[$randomChoice]);
 	      	header("HTTP/1.1 200 OK");
 	      return $data;
+  		}
+      	$data = [];
+      	if(isset($request->url_elements[2]) && $request->url_elements[2]  != '') {
+	        $data = $request->url_elements[2];
+
     	}
 	}
 }
