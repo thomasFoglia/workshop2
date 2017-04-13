adade9f6355c44ea7beafe83d11672466d344e25
Thomas Foglia <thomas.foglia38@gmail.com>
2017-04-13 11:30:14 +0200

    ia LIGNE

--- testia.php
+++ testia.php
@@ -5,14 +5,14 @@ $numJoueur = 1;
 
 $currentGrid = [
 //  0   1  2  3  4  5  6  7  8  9 10 11  12 13 14 15 16 17 18
-    [0, 0, 0, 0, 0, 2, 2, 1, 0, 1 ,1 ,1 , 0, 0, 0, 0, 0, 0, 0],//0
+    [0, 0, 1, 0, 0, 2, 2, 1, 0, 1 ,0 ,1 , 1, 1, 1, 0, 0, 0, 0],//0
     [0, 0, 0, 0, 2, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//1
     [0, 0, 0, 0, 2, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//2
     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//3
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//4
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//5
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//6
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//7
+    [0, 1, 1, 0, 0, 1, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//7
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//8
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//9
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0],//10
@@ -26,48 +26,51 @@ $currentGrid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 , 0, 0, 0, 0, 0, 0, 0]//18
 ];
 
-
-$pts = [];
-$i = 0;
+// parcourt la grid
 foreach($currentGrid as $ligne) {
-    $j = 0;
-    foreach($ligne as $cell) {
-        if ($cell == $numJoueur) {
-            $pts[] = [$i, $j]; // X ; Y
-        }
-        $j++;
-    }
-    $i++;
-}
-//var_dump($pts);
-checkLine($pts);
-
-function checkLine($pts_user) {
-  $datas_grouped = array();
-  foreach ($pts_user as $p) {
-    if (!isset($datas_grouped[$p[0]])) {
-      $datas_grouped[$p[0]] = array($p[1]);
-    } else {
-      $datas_grouped[$p[0]][] = $p[1];
-    }
-    rsort($datas_grouped[$p[0]]);  // tri ordonnées
-  }
-  //var_dump($datas_grouped);
-
-  foreach ($datas_grouped as $abs => $ordonnees) {
-    var_dump($abs);
-    var_dump($ordonnees);
-    $size_ords = count($ordonnees);
-    var_dump($size_ords . " éléments");
-    if ($size_ords > 1) {
-      foreach ($ordonnees as $ord) {
-        var_dump($ord);
+  $width = 0;
+  $highest = [];
+  $init_y = -1;
+  $l_inc = 0;
+  // parcourt la ligne
+  foreach ($ligne as $key => $value) {
+    // c'est la bonne valeur
+    if ($value == $numJoueur) {
+      if ($init_y == -1 || $wrong == 1) {
+        $init_y = $key;
       }
+      $end_y = $key;
+      $width ++;
+      $highest[] = array("init_y" => $init_y, "end_y" => $end_y, "width" => $width);
+      $wrong = 0;
     } else {
-      // peut pas être exploité car 1 seul élément
+      // pas la bonne valeur
+      // on retinitialise le comptage
+      if ($width != 0) {
+        $wrong = 1;
+        $width--;
+      }
     }
-  echo('<br>_________________________________<br>');
   }
 
+  $highest = getHighestSerie($highest);
+  $highest_all[] = $highest; // va contenir toutes les series max de chaque ligne
+  $l_inc++;
 }
+
+var_dump($highest_all);
+
+// retourne la plus grande série de la ligne
+function getHighestSerie($array) {
+  $h = 0;
+  $final = [];
+  foreach ($array as $a) {
+    if ($a["width"] > $h) {
+      $final = $a;
+      $h = $a["width"];
+    }
+  }
+  return $final;
+}
+
 ?>
